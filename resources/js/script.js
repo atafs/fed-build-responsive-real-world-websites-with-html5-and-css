b7006059befc8a25da7a9a3aadbbce92e4eedc55
atafs <americoviagens@gmail.com>
2017-08-28 16:13:54 +0100

    66: Adding animation on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   
-    /* For the sticky navigation */  
+
+    /* For the sticky navigation */
     var waypoint = new Waypoint({
       element: document.getElementsByClassName('js--section-features'),
       handler: function(direction) {
@@ -9,37 +9,68 @@ $(document).ready(function() {
           } else {
             $('nav').removeClass('sticky');
           }
-      }, 
+      },
       offset: 150
     });
-    
+
     /* Scroll of bottons */
     $('.js--scroll-to-plans').click(function() {
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1500);
     });
-    
+
      $('.js--scroll-to-start').click(function() {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 700);
     });
-    
+
     /* Navigation scroll */
-    // Scroll to specific values; scrollTo is the same;
-    window.scroll({
-      top: 2500, 
-      left: 0, 
-      behavior: 'smooth' 
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
     });
 
-    // Scroll certain amounts from current position 
-    window.scrollBy({ 
-      top: 100, // could be negative value
-      left: 0, 
-      behavior: 'smooth' 
-    });
+    /* Animation */
+    var waypoint = new Waypoint({
+      element: document.getElementsByClassName('js--wp-1'),
+      handler: function(direction) {
+         $('.js--wp-1').addClass('animated fadeIn');
+      },
+      offset: '50%'
+    })
 
-    // Scroll to a certain element
-    document.querySelector('.hello').scrollIntoView({ 
-      behavior: 'smooth' 
-    });
-        
+    var waypoint = new Waypoint({
+      element: document.getElementsByClassName('js--wp-2'),
+      handler: function(direction) {
+         $('.js--wp-2').addClass('animated fadeInUp');
+      },
+      offset: '50%'
+    })
+
+    var waypoint = new Waypoint({
+      element: document.getElementsByClassName('js--wp-3'),
+      handler: function(direction) {
+         $('.js--wp-3').addClass('animated fadeIn');
+      },
+      offset: '50%'
+    })
+
+    var waypoint = new Waypoint({
+      element: document.getElementsByClassName('js--wp-4'),
+      handler: function(direction) {
+         $('.js--wp-4').addClass('animated pulse');
+      },
+      offset: '50%'
+    })
+
+    /* Parallax */
+    $('#main').stellar();
 });
